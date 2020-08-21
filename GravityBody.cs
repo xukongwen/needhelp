625e0e1effe187a631a044574d3900d92d374638
徐空文 <oabdao@gmail.com>
2020-08-21 13:42:46 +0800

    hi
    hi

---
+++ GravityBody.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityBody : GravityObject
+{
+    public Rigidbody rb;
+    Quaternion targetRot;
+	Quaternion smoothedRot;
+
+    void Awake () {
+		InitRigidbody ();
+		targetRot = transform.rotation;
+		smoothedRot = transform.rotation;
+
+	}
+    void FixedUpdate () {
+		// Gravity
+		Vector3 gravity = NBodySimulation.CalculateAcceleration (rb.position);
+		rb.AddForce (gravity, ForceMode.Acceleration);
+
+	}
+
+    void InitRigidbody () {
+		rb = GetComponent<Rigidbody> ();
+		rb.interpolation = RigidbodyInterpolation.Interpolate;
+		rb.useGravity = false;
+		rb.isKinematic = false;
+		rb.centerOfMass = Vector3.zero;
+		rb.collisionDetectionMode = CollisionDetectionMode.ContinuousSpeculative;
+	}
+}
---
+++ Orbiter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class Orbiter : MonoBehaviour {
+
+
+    public Vector3 force;
+
+    public Rigidbody rb;
+
+    private void Awake() {
+        
+        
+        
+        
+        TrajectoryPredictor trajectory = GetComponent<TrajectoryPredictor>();        
+        trajectory.OnPredictionIterationStep = HandlePredictionGravity;
+        
+
+        rb = GetComponent<Rigidbody>();
+        
+        
+        
+    }
+
+    void HandlePredictionGravity(ref Vector3 currentIterationVel, Vector3 currentIterationPos, TrajectoryPredictor tpInstance) {
+        force = NBodySimulation.CalculateAcceleration(currentIterationPos);
+        
+        currentIterationVel = force;
+        //Debug.DrawRay(transform.position, force.normalized,Color.red);
+  
+  
+    }
+
+
+}
